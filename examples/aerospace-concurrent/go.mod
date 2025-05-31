0abf2fd5ee81eab88ce302600ef2b53e1981dfe9
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-31 13:17:58 +0200

    feat: makes connection concurrent safe

---
+++ examples/aerospace-concurrent/go.mod
@@ -0,0 +1,5 @@
+module github.com/cristianoliveira/aerospace-concurrent
+
+go 1.24.2
+
+require github.com/cristianoliveira/aerospace-ipc v0.0.2-0.20250531111758-5dfd9fd16125 // indirect
---
+++ examples/aerospace-concurrent/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"sync"
+
+	client "github.com/cristianoliveira/aerospace-ipc"
+)
+
+func main() {
+	conn, err := client.NewAeroSpaceConnection()
+	if err != nil {
+		log.Fatalf("Error creating connection: %v", err)
+	}
+
+	var wg sync.WaitGroup
+	numGoroutines := 5
+
+	for i := range numGoroutines {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			response, err := conn.Client().SendCommand("list-windows", []string{"--all", "--json"})
+			if err != nil {
+				log.Printf("Goroutine %d: Error sending command: %v", id, err)
+				return
+			}
+			log.Printf("Goroutine %d: Received response: %v", id, response)
+		}(i)
+	}
+
+	wg.Wait()
+}
