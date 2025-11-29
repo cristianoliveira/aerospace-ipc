ad9fb86248a663d24d4e734052eefe8d629422ed
cristianoliveira <public+github@cristianoliveira.com>
2025-11-29 15:24:20 +0100

    refactor: remove old interface and explain migration

--- tests/main_test.go
+++ tests/main_test.go
@@ -5,17 +5,18 @@ package tests
 import (
 	"testing"
 
-	ipc "github.com/cristianoliveira/aerospace-ipc"
+	"github.com/cristianoliveira/aerospace-ipc/pkg/aerospace"
 )
 
 func TestIntegration(t *testing.T) {
-	t.Run("ipc.GetAllWindows: retrieves all windows", func(t *testing.T) {
-		client, err := ipc.NewAeroSpaceClient()
+	t.Run("aerospace.Windows().GetAllWindows: retrieves all windows", func(t *testing.T) {
+		client, err := aerospace.NewClient()
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
 		}
+		defer client.CloseConnection()
 
-		windows, err := client.GetAllWindows()
+		windows, err := client.Windows().GetAllWindows()
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
 		}
@@ -26,11 +27,12 @@ func TestIntegration(t *testing.T) {
 		t.Logf("Retrieved %d windows", len(windows))
 	})
 
-	t.Run("ipc.Client().GetSocketPath: retrieves the socket path", func(t *testing.T) {
-		client, err := ipc.NewAeroSpaceClient()
+	t.Run("aerospace.Client().Connection().GetSocketPath: retrieves the socket path", func(t *testing.T) {
+		client, err := aerospace.NewClient()
 		if err != nil {
 			t.Fatalf("expected no error, got %v", err)
 		}
+		defer client.CloseConnection()
 
 		path, err := client.Connection().GetSocketPath()
 		if err != nil {
